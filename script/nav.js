f39c44427d5b9710c3fc7602056cfe939daf07b7
محمد ابراهيم محمد الحصري <melhosary178@gmail.com>
2025-12-19 00:29:30 +0200

    Delete script/nav.js

--- script/nav.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector(".burger");
-if (burger) {
-  burger.addEventListener("click", () => {
-    menu.classList.toggle("active");
-    burger.textContent = menu.classList.contains("active") ? "✖" : "☰";
-  });
-}
